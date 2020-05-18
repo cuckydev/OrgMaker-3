aeb7a28d5d44f19156b321ac37a56e1cb30a7b6a
cuckydev <cuckydev@users.noreply.github.com>
2020-05-18 03:05:49 -0400

    init

---
+++ build/CMakeFiles/OrgMaker3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OrgMaker3.dir/src/Main.cpp.o"
+  "OrgMaker3"
+  "OrgMaker3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OrgMaker3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/external/SDL2/CMakeFiles/SDL2main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SDL2main.dir/src/main/dummy/SDL_dummy_main.c.o"
+  "libSDL2main.a"
+  "libSDL2main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SDL2main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
